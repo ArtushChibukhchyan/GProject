4a33e3f388101b807ed081a4e3e6e2a1f59e4a59
artush <artush@flowfit.com>
2021-06-13 19:42:47 +0400

    first commit

---
+++ Gaming/Gaming/Controllers/V1/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using Gaming.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gaming.Controllers.V1
+{
+    public class AuthenticationController : Controller
+    {
+
+        public IActionResult Register(UserRegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = string.Join(',', ModelState.Select(x => x.Value.Errors)
+                        .Where(y => y.Count > 0)
+                        .ToList());
+                return Ok(errors);
+            }
+
+            return Ok("Success");
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gaming/Gaming/Models/UserRegisterRequest.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gaming.Models
+{
+    public class UserRegisterRequest
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+
+        [Required]
+        public int RegionId { get; set; }
+        
+        
+        [Required]
+        public int CityId { get; set; }
+
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+    }
+}
